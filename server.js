209e1060c3df8e37af0411f4484994b5e72902bf
gitzdave <gitzdave@yahoo.com>
2021-12-08 16:01:46 +0000

    2 files modified, 1 added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
 
 http.createServer (function (req, res){
-
+  console.log("Server running")
   res.write ("Hey welcome, i have created my first server");
   res.end ();
 
