d4933563c11ba96a667cb69277134e5bbef10af5
gitzdave <gitzdave@yahoo.com>
2021-12-14 15:56:43 +0000

    1 file modified

--- server-hello.js
+++ server-hello.js
@@ -4,20 +4,14 @@ var app = express();
 app.use(cors());
 
 
-
-var server = app.listen ('8080', function (){
-  console.log ("Hello connecting the data")
+server = app.listen ('8080', function (){
+  console.log ("Hello connecting and listening");
 //var host = server.adress().adress;
 //var port = server.adress().port;
 
-})
+});
 
-app.get ("./hello", function (req, res){
-res.write ("Hello people from server side")
-res.end ();
-
-
-//pp.get ("./hello", function (req, res){
-  //res.write ("Hello people")
-  
-  })
+app.get ('/', function (req, res){
+  res.write("Hello people");
+  res.end();
+});
